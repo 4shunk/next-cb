d25b897e797bb79f834859479f2551dbd5826d01
4shunk <74296170+4shunk@users.noreply.github.com>
2020-11-26 15:35:13 +0900

    Image Optimization test

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,19 @@
-import Link from 'next/link'
+import Link from "next/link";
+import Image from "next/image";
 
 export default function IndexPage() {
   return (
     <div>
-      Hello World.{' '}
+      Hello World.{" "}
       <Link href="/about">
         <a>About</a>
       </Link>
+      <Image
+        width={1500}
+        height={1000}
+        layout="responsive"
+        src="https://images.unsplash.com/photo-1606248627653-f3d6b573deb8"
+      />
     </div>
-  )
+  );
 }
